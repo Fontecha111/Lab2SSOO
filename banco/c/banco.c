80d79d7deb6b97fce4fccdc03cd99e40fbc84417
Fontecha111 <9404133@alumnos.ufv.es>
2026-04-16 18:35:20 +0200

    Tercera parte, id de cuenta

--- banco/c/banco.c
+++ banco/c/banco.c
@@ -28,33 +28,66 @@ void lanzar_monitor() {
     }
 }
 
+int crear_nueva_cuenta()
+{
+    sem_t *sem_config = sem_open("/sem_config", 0);
+    sem_t *sem_cuentas = sem_open("/sem_cuentas", 0);
+
+    sem_wait(sem_config);
+
+    int nuevo_id = config_banco.proximo_id;
+    config_banco.proximo_id++;
+
+
+    sem_post(sem_config);
+
+    Cuenta nueva_cuenta;
+    nueva_cuenta.numero_cuenta = nuevo_id;
+    sprintf(nueva_cuenta.titular, "Usuario %d", nuevo_id);
+    nueva_cuenta.saldo_eur = 0.0;
+    nueva_cuenta.saldo_usd = 0.0;
+    nueva_cuenta.saldo_gbp = 0.0;
+    nueva_cuenta.num_transacciones = 0;
+
+    sem_wait(sem_cuentas);
+
+    FILE *archivo = fopen(config_banco.archivo_cuentas, "ab");
+    if(archivo != NULL)
+    {
+        fwrite(&nueva_cuenta, sizeof(Cuenta), 1, archivo);
+        fclose(archivo);
+    }
+    else
+    {
+        perror("Error al intentar abrir el archivo");
+    }
+
+    sem_post(sem_cuentas);
+
+    printf("La cuenta se ha creado exitosamente. Tu número de cuenta es: %d\n", nuevo_id);
+
+    return nuevo_id;
+}
+
 void bucle_principal() {
     while (1) {
         int cuenta;
 
-        printf("Introduce número de cuenta (0 para crear nueva): ");
-        fflush(stdout);
+        printf("\nIntroduce número de cuenta (0 para crear nueva): ");
+        scanf("%d", &cuenta);
 
-        if (scanf("%d", &cuenta) != 1) {
-            fprintf(stderr, "Entrada inválida\n");
-            exit(1);
+        if(cuenta == 0)
+        {
+            cuenta = crear_nueva_cuenta();
         }
 
         int pipefd[2];
-        if (pipe(pipefd) == -1) {
-            perror("Error creando pipe");
-            exit(1);
-        }
+        pipe(pipefd);
 
         pid_t pid = fork();
-        if (pid < 0) {
-            perror("Error en fork");
-            exit(1);
-        }
 
         if (pid == 0) {
             close(pipefd[1]);
-            close(pipefd[0]);
 
             char cuenta_str[20];
             sprintf(cuenta_str, "%d", cuenta);
@@ -66,7 +99,6 @@ void bucle_principal() {
             exit(1);
         } else {
             close(pipefd[0]);
-            close(pipefd[1]);
 
             waitpid(pid, NULL, 0);
         }
